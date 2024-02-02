10ecc67cca082d21f8852d5e57f0329b71220df7
novr (Nobuhisa Komiya) <n_komiya@yumemi.co.jp>
2024-02-02 17:22:44 +0900

    Initial Commit

---
+++ swift-openapi-generator-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-openapi-generator-test
+//
+//  Created by 古宮 伸久 on 2024/02/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-openapi-generator-test/swift_openapi_generator_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_openapi_generator_testApp.swift
+//  swift-openapi-generator-test
+//
+//  Created by 古宮 伸久 on 2024/02/02.
+//
+
+import SwiftUI
+
+@main
+struct swift_openapi_generator_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
