4ae673835a7137f6141a7cb5503be984bb702ebf
novr (Nobuhisa Komiya) <n_komiya@yumemi.co.jp>
2024-02-02 20:02:37 +0900

    feat: ci_scripts

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  swift-openapi-generator-test
+#
+#  Created by 古宮 伸久 on 2024/02/02.
+#  
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
